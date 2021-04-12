53b3fe493fa9dee99f6f84169cb16f29cefbd3d8
ChoHyunBin12 <80668234+ChoHyunBin12@users.noreply.github.com>
2021-04-12 15:07:38 +0900

    0412

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class ScrollingObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!GameManager.instance.isGameover)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
